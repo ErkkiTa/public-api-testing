56945c707351d88d20fc4024d9bec41b565dc7dd
ErkkiTa <erkkitamb5@gmail.com>
2020-02-28 09:25:51 +0200

    tehtud

--- api.js
+++ api.js
@@ -1,17 +1,22 @@
 
-fetch('https://official-joke-api.appspot.com/random_joke')
-.then(res => {
-    return res.json()
-})
+function getJoke() {
+    
 
-.then(data => {
-    const container = document.getElementById('container')
-    console.log(data.setup)
-    console.log(data.punchline)
-    container.innerHTML ='<div class="algus">' + data.setup + '</div> <div class="lopp">' + data.punchline + '</div>'
-})
+    fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(res => {
+        return res.json()
+    })
+
+    .then(data => {
+        const container = document.getElementById('container')
+        console.log(data.setup)
+        console.log(data.punchline)
+        container.innerHTML ='<div class="algus">' + data.setup + '</div> <div class="lopp">' + data.punchline + '</div>'
+    })
 
 
+}
+document.getElementById('aamen').addEventListener("click", getJoke)
 
-document.getElementById('aamen').addEventListener("click", )
+getJoke()
 
